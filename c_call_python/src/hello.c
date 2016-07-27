430ec1fb5a39b21ef0b3cadc7365ca62b85f9ff0
ivan0124 <ivan0124.tw@gmail.com>
2016-07-27 11:29:14 +0800

    add include python headers

--- c_call_python/src/hello.c
+++ c_call_python/src/hello.c
@@ -1,8 +1,10 @@
 /*hello.c*/
 #include <stdio.h>
+#include "/usr/include/python2.7/Python.h"
 
 int main( int argc, char* argv[])
 {
+    PyObject *pName;
     printf("Hello World\n");
     fnA();
     return 0;
