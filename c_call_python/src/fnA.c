5251f064c54079bd30e495b6226a48dd7e5b4176
ivan0124 <ivan0124.tw@gmail.com>
2016-07-27 11:09:18 +0800

    add c_call_python test

---
+++ c_call_python/src/fnA.c
@@ -0,0 +1,8 @@
+/*fnA.c*/
+#include <stdio.h>
+
+int fnA()
+{
+    printf("fnA() in called\n");
+    return 0;
+}
---
+++ c_call_python/src/hello.c
@@ -0,0 +1,9 @@
+/*hello.c*/
+#include <stdio.h>
+
+int main( int argc, char* argv[])
+{
+    printf("Hello World\n");
+    fnA();
+    return 0;
+}
