e3546c5e2a118c10637fcbe810582008448e8472
ivan0124 <ivan0124.tw@gmail.com>
2016-08-09 13:38:35 +0800

    update mk.sh

--- MyMqttDrv/mk.sh
+++ MyMqttDrv/mk.sh
@@ -4,4 +4,6 @@ cp -rf ./src/ /mnt/WISE_IOT/NetDevice/Mqtt/
 cd /mnt/WISE_IOT/NetDevice/Mqtt/
 make clean
 make
-
+sudo cp -f /mnt/WISE_IOT/NetDevice/Mqtt/src/.libs/libMqttDrv.so.1.0.0 /usr/lib/Advantech/iotgw/
+sudo cp -f /mnt/WISE_IOT/NetDevice/Mqtt/src/.libs/libMqttDrv.a /usr/lib/Advantech/iotgw/
+sudo cp -f /mnt/WISE_IOT/NetDevice/Mqtt/src/.libs/libMqttDrv.la /usr/lib/Advantech/iotgw/
