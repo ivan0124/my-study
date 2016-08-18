f15c7d1d28271e11ba433129b94e3967f5d56217
ivan0124 <ivan0124.tw@gmail.com>
2016-08-18 15:37:43 +0800

    node-red call python

---
+++ create_NodeRed_node/hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+def main():
+    print ("[Python] Hello, World!")
+
+
+if __name__  == "__main__":
+    main()
--- create_NodeRed_node/lower-case.js
+++ create_NodeRed_node/lower-case.js
@@ -3,7 +3,34 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         this.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
+            //msg.payload = msg.payload.toLowerCase();
+            msg.payload = "123";
+            console.log('my test');
+            console.log(process.cwd());
+
+            var PythonShell = require('python-shell');
+            var pyshell = new PythonShell('hello.py');
+
+            // sends a message to the Python script via stdin
+            //pyshell.send('hello');
+
+            pyshell.on('message', function (message) {
+                // received a message sent from the Python script (a simple "print" statement)
+                console.log('my message:');
+                console.log(message);
+            });
+
+            // end the input stream and allow the process to exit
+            pyshell.end(function (err) {
+                if (err) throw err;
+                console.log('my_test:finished');
+            });
+/*
+            PythonShell.run('hello.py', function (err) {
+                if (err) throw err;
+                console.log('finished');
+            });
+*/
             node.send(msg);
         });
     }
