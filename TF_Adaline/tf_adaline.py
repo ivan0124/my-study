03b186ce1dac2028e5111604e691c98da965eb9a
ivan0124 <ivan0124.tw@gmail.com>
2016-08-24 18:01:50 +0800

    update code

--- TF_Adaline/tf_adaline.py
+++ TF_Adaline/tf_adaline.py
@@ -2,21 +2,31 @@
 
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 
-X = tf.placeholder(tf.float32, [3, 2])
+df = pd.read_csv('./sensor.data',header=None)
+df.tail();
+
+#y = df.iloc[0:99, 2].values
+#y = np.where(y == 'Abnormal', -1, 1)
+#x_data = df.iloc[0:99, [0,1]].values
+
+#print ("x_data=%s" % (x_data))
+
+X = tf.placeholder(tf.float32, [None, 2])
 W = tf.Variable(tf.zeros([2, 1]))
-#b = tf.Variable(tf.zeros([3,1]))
+b = tf.Variable(tf.zeros([1]))
 
 #X = tf.constant([[1., 1.],[2.,2.],[3.,3.]])
 #W = tf.constant([[3.],[4.]])
-y_pred = tf.matmul(X, W) 
+y_pred = tf.matmul(X, W)+b
 
 #correct answer
-y_ =  tf.placeholder(tf.float32, [3, 1])
+y_ =  tf.placeholder(tf.float32, [None, 1])
 
 #
 loss = tf.reduce_sum(tf.square(y_ - y_pred))
-optimizer = tf.train.GradientDescentOptimizer(0.001)
+optimizer = tf.train.GradientDescentOptimizer(0.0001)
 train = optimizer.minimize(loss)
 
 # Before starting, initialize the variables.  We will 'run' this first.
@@ -32,10 +42,15 @@ merged_summary_op = tf.merge_all_summaries()
 summary_writer = tf.train.SummaryWriter('./logs', sess.graph)
 
 
-for i in range(100):
+for i in range(1000):
   x_data=[[1.,1.], [2.,2.], [3.,3.]]
   y_answer=[[1.],[-1.],[1.]]
+  #x_data = df.iloc[0:99, [0,1]].values
+  #y_answer = df.iloc[0:99, [2]].values
+  #print ("y_answer=%s" % y_answer)
   sess.run(train, feed_dict={X: x_data, y_: y_answer})
   summary_str = sess.run(merged_summary_op, feed_dict={X: x_data, y_: y_answer}) 
   summary_writer.add_summary(summary_str, i)
-  
+ 
+
+print sess.run(W) 
