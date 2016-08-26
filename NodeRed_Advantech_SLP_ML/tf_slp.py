63668bd9ab0a1b6ca9079f09f5963b5915bce2ea
ivan0124 <ivan0124.tw@gmail.com>
2016-08-26 17:52:59 +0800

    add plot data

--- NodeRed_Advantech_SLP_ML/tf_slp.py
+++ NodeRed_Advantech_SLP_ML/tf_slp.py
@@ -10,6 +10,24 @@ import matplotlib.pyplot as plt
 ipd = pd.read_csv("./iris.csv")
 ipd.head()
 
+'''
+plt.subplot(2,1,1)
+for key,val in ipd.groupby('Species'):
+    plt.plot(val['sepal_length'], val['sepal_width'], label=key, linestyle="none",  marker='.')
+plt.xlabel('Sepal Length')
+plt.ylabel('Sepal Width')
+
+
+plt.subplot(2,1,2)
+for key,val in ipd.groupby('Species'):
+    plt.plot(val['petal_length'], val['petal_width'], label=key, linestyle="none",  marker='.')
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')   
+
+plt.legend(loc='best')
+plt.show()
+'''
+
 species = list(ipd['Species'].unique())
 ipd['One-hot'] = ipd['Species'].map(lambda x: np.eye(len(species))[species.index(x)] )
 #print ("ipd.sample(5)=\n%s" % (ipd.sample(5)))
