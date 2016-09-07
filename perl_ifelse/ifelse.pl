ea9f1c0e375ae004d3c21dbd9a8b22ebe70e29c1
ivan0124 <ivan0124.tw@gmail.com>
2016-09-07 13:35:57 +0800

    update code

--- perl_ifelse/ifelse.pl
+++ perl_ifelse/ifelse.pl
@@ -7,4 +7,18 @@ if ( "$a" == "100" ){
 }
 else{
     print "\$a != 100\n";
-} 
+}
+
+# 如果指令(statement)只有一項，我們可以使用倒裝句法，看起來比較簡潔。
+#statement if (Expression);
+$name="friend";
+print "HELLO!\n" if ($name eq "friend");
+#
+$x=100;
+$x-=10 if ($x == 100);
+print "x=$x\n";
+ 
+# unless 就是if not
+#statement unless (Expression);
+$test="testaa";
+print "Hello, not a test\n" unless ($test eq "test");
