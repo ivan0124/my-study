f5618c0e6af26f265df110e6df3b89903d237bf3
ivan0124 <ivan0124.tw@gmail.com>
2016-09-07 13:28:16 +0800

    update code

--- perl_ifelse/ifelse.pl
+++ perl_ifelse/ifelse.pl
@@ -1,29 +1,10 @@
 #!/usr/bin/perl
 
 #
-@array=("a","b","c","d");
-
-print "run for loop ===>\n";
-for($i=0; $i<=$#array; $i++) {
-    print "array[$i]=$array[$i]\n";
+$a=1001;
+if ( "$a" == "100" ){
+    print "\$a == 100\n";
 }
-
-#
-print "run foreach loop ===>\n";
-foreach $i (@array) {
-    print "$i\n";
-}
-
-#
-print "run foreach loop(0..3) ===>\n";
-foreach $i (0..3) {
-    print "array[$i]=$array[$i]\n";
-}
-
-#
-print "while loop ===>\n";
-$i=0;
-while($i<=3) {
-    print "array[$i]=$array[$i]\n";
-    $i++;
-}
+else{
+    print "\$a != 100\n";
+} 
