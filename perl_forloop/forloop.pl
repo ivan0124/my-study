4505901d64483b5472fed621c4f6f71ad35e4dbf
ivan0124 <ivan0124.tw@gmail.com>
2016-09-07 13:53:36 +0800

    update code

--- perl_forloop/forloop.pl
+++ perl_forloop/forloop.pl
@@ -27,3 +27,18 @@ while($i<=3) {
     print "array[$i]=$array[$i]\n";
     $i++;
 }
+
+# last是跳出現在所在的迴圈，next則是跳過下面的指令直接執行下一次的迴圈。
+$i=0;
+while($i<=20) {
+    $i++;
+    if ($i <= 5){
+        print "continue loop. i=$i\n";
+        next;
+    }
+    print "i=$i\n";
+    if ($i > 10){
+        print "exit loop. i=$i\n";
+        last;
+    }
+}
