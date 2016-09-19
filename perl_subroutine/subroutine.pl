1ddcc6e397c7c402d1a7f553600b8433152ee819
ivan0124 <ivan0124.tw@gmail.com>
2016-09-19 13:23:06 +0800

    update code

--- perl_subroutine/subroutine.pl
+++ perl_subroutine/subroutine.pl
@@ -45,3 +45,33 @@ my $test_val=20;
 reassign($test_val);
 
 print "test_val=$test_val\n";
+
+=head1
+Sample4: function parameters can be changed.
+=cut
+
+sub list_mote
+{
+    #2 dimension array
+    local @plist;
+    $plist[0]->{"mac"} = "11:22:33:44:55:66";
+    $plist[0]->{"moteid"} = "1";
+    $plist[1]->{"mac"} = "22:33:44:55:66:66";
+    $plist[1]->{"moteid"} = "2";
+
+    #print "MAC=$plist[0]->{'mac'}\n";
+    #print "Mote ID=$plist[0]->{'moteid'}\n";
+    #print "MAC=$plist[1]->{'mac'}\n";
+    #print "Mote ID=$plist[1]->{'moteid'}\n";
+    return @plist;
+}
+
+@motes=&list_mote();
+print "@motes\n";
+
+foreach $pr (@motes) {              
+    local @cols;          
+    push(@cols, $pr->{'mac'});   
+    push(@cols, $pr->{'moteid'});
+    print "@cols\n";
+} 
