576fbb22d1db57a58e902235a2dba724ca1051aa
ivan <ivan0124.tw@gmail.com>
2016-09-21 18:04:08 +0800

    update code

--- perl_mqtt_client_server/mqtt-client.pl
+++ perl_mqtt_client_server/mqtt-client.pl
@@ -16,6 +16,23 @@ JSON: test data
 
 sub nr_wsn
 {
+        my %WSN_topic;
+        #
+        $WSN_topic{"WSN"}{"Set"}{"NetID"}{"Request"} = "/WSNMgmt/IoTGW/WSN/SetNetID_Request/";
+        $WSN_topic{"WSN"}{"Set"}{"NetID"}{"Response"} = "/WSNMgmt/IoTGW/WSN/SetNetID_Response/";
+        $WSN_topic{"WSN"}{"Get"}{"NetID"}{"Request"} = "/WSNMgmt/IoTGW/WSN/GetNetID_Request/";
+        $WSN_topic{"WSN"}{"Get"}{"NetID"}{"Response"} = "/WSNMgmt/IoTGW/WSN/GetNetID_Response/";
+        #
+        $WSN_topic{"WSN"}{"Set"}{"JoinKey"}{"Request"} = "/WSNMgmt/IoTGW/WSN/SetJoinKey_Request/";
+        $WSN_topic{"WSN"}{"Set"}{"JoinKey"}{"Response"} = "/WSNMgmt/IoTGW/WSN/SetJoinKey_Response/";
+        $WSN_topic{"WSN"}{"Get"}{"JoinKey"}{"Request"} = "/WSNMgmt/IoTGW/WSN/GetJoinKey_Request/";
+        $WSN_topic{"WSN"}{"Get"}{"JoinKey"}{"Response"} = "/WSNMgmt/IoTGW/WSN/GetJoinKey_Response/";
+        #
+        print "---------------\n";
+        print "$WSN_topic{'WSN'}{'Set'}{'JoinKey'}{'Request'}\n";
+        print "---------------\n";
+      
+
 	my $mqtt =  Net::MQTT::Simple->new($BROKER);
 	#my $json_obj = JSON->new;
 	my $nr = 0;
