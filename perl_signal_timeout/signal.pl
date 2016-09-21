809333de42cb20c0102f41d23fbc10f602c3d114
ivan <ivan0124.tw@gmail.com>
2016-09-21 10:05:54 +0800

    update code

--- perl_signal_timeout/signal.pl
+++ perl_signal_timeout/signal.pl
@@ -2,9 +2,25 @@
 
 =pod
 comment here:
-This is a hello perl.
-Welcome !!
 =cut
 
-$var=100;
-print "Hello World! var=$var\n";
+
+$timeout=3;
+eval{
+    local $SIG{ALRM} = sub { die "timeout occur:$timeout second, run alarm\n"; };
+    alarm $timeout; #設定3秒後進入超時處理, function 定義在 $SIG{ALRM}
+    sleep 20;
+    print "in alarm eval\n";
+    alarm(0); #取消超時處理 
+};
+print "out of alarm eval\n";
+
+#處理超時function中的打印會放在 $@中
+if($@){
+    if($@ =~ /die in sig alarm/){
+        print "caught the error\n";
+    }
+    else{
+        print $@;
+    }
+}
