9b12ba100c56cb15deaa6e1e0c5eb18213e247be
ivan <ivan0124.tw@gmail.com>
2016-09-24 09:53:03 -0500

    Update hello.js

--- NodeJs_Express_hello/hello.js
+++ NodeJs_Express_hello/hello.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.get('/', function (req, res) {
   // "Hello"
-  greetings.sayHelloInEnglish();
+  console.log(greetings.sayHelloInEnglish());
   // "Hola"  
   greetings.sayHelloInSpanish();  
   res.send('Hello World!');
