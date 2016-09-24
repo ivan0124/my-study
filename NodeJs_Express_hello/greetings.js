389d5fa5a3ec00655a729f6d348a00a4210bd890
ivan <ivan0124.tw@gmail.com>
2016-09-24 09:49:00 -0500

    Update greeting.js

--- NodeJs_Express_hello/greetings.js
+++ NodeJs_Express_hello/greetings.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+module.exports = {
+  sayHelloInEnglish: function() {
+    return "HELLO";
+  },
+       
+  sayHelloInSpanish: function() {
+    return "Hola";
+  }
+};
