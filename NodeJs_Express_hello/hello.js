5e44b7e7584a1509f0652fc037c53e0a656c6d4f
ivan <ivan0124.tw@gmail.com>
2016-09-25 16:30:22 +0800

    Update hello.js

--- NodeJs_Express_hello/hello.js
+++ NodeJs_Express_hello/hello.js
@@ -11,6 +11,11 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/test', function (req, res) {  
+  res.send('/test/Hello World!');
+});
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!!!!');
 });
