b7eacdcde5a7433149ed2e3293d159da2529e7c9
ivan <ivan0124.tw@gmail.com>
2016-09-27 00:39:57 +0800

    Update hello.js

--- NodeJs_Express_hello/hello.js
+++ NodeJs_Express_hello/hello.js
@@ -31,7 +31,7 @@ app.get('/json', function (req, res) {
    
    //
    var string = JSON.stringify(susiObj.susiCommData.infoSpec.IoTGW.WSN.WSN0.Info);
-   var jsonObj = JSON.parse(string);
+   var object = JSON.parse(string);
    JSONObject getObject = object.getJSONObject("Info");
   
    for (var i = 0; i < susiObj.susiCommData.infoSpec.IoTGW.WSN.WSN0.Info.e.length; i++) { 
