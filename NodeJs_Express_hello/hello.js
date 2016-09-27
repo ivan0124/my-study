5ef7f0fd72c4a47c53c94086087864b6724cb304
ivan <ivan0124.tw@gmail.com>
2016-09-27 04:18:20 -0500

    Update hello.js

--- NodeJs_Express_hello/hello.js
+++ NodeJs_Express_hello/hello.js
@@ -3,6 +3,21 @@ var greetings = require("./greetings.js");
 var HashMap = require('hashmap').HashMap;
 var map = new HashMap();
 var app = express();
+//Mqtt
+var mqtt = require('mqtt');
+var client  = mqtt.connect('mqtt://test.mosquitto.org');
+
+client.on('connect', function () {
+  client.subscribe('presence')
+  client.publish('presence', 'Hello mqtt')
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer 
+  console.log(message.toString())
+  client.end()
+})
+
 
 app.get('/', function (req, res) {
   // "Hello"
