13ef2fd42af9b7129cabef3b029916cadb670012
ivan <ivan0124.tw@gmail.com>
2016-09-27 11:41:53 +0800

    Update hello.js

--- NodeJs_Express_hello/hello.js
+++ NodeJs_Express_hello/hello.js
@@ -52,7 +52,7 @@ app.get('/restapi/susiCommData/infoSpec/IoTGW', function (req, res) {
    var cStr="infoSpec";
    console.log(" susiCommData.sendTS ----------------> " + susiObj[aStr][bStr] + ",type = " + typeof susiObj[aStr][bStr]);
   
-   console.log(" susiCommData.sendTS ----------------> " + susiObj[aStr][cStr] + ", type = " + typeof susiObj[aStr][cStr]);
+   console.log(" susiCommData.infoSpec ----------------> " + susiObj[aStr][cStr] + ", type = " + typeof susiObj[aStr][cStr]);
    //
    for (key in susiObj) {
        if (susiObj.hasOwnProperty(key)) {
