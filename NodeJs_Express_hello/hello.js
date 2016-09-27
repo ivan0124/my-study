550229e78ae2d72bb06f5926d6724f4ecd4ee6df
ivan <ivan0124.tw@gmail.com>
2016-09-27 10:38:58 +0800

    Update hello.js

--- NodeJs_Express_hello/hello.js
+++ NodeJs_Express_hello/hello.js
@@ -50,8 +50,10 @@ app.get('/restapi/susiCommData/infoSpec/IoTGW', function (req, res) {
    var string = JSON.stringify(susiObj.susiCommData.infoSpec.IoTGW.WSN.WSN0.Info);
    console.log("string="+string); 
   
+   console.log("=========>"+susiObj.susiCommData.infoSpec.IoTGW.WSN.WSN0.Info);
+  
    for (var i = 0; i < susiObj.susiCommData.infoSpec.IoTGW.WSN.WSN0.Info.e.length; i++) { 
-       console.log(susiObj.susiCommData.infoSpec.IoTGW.WSN.WSN0.Info.e[i].n); 
+       console.log(susiObj.susiCommData.infoSpec.IoTGW.WSN.WSN0.Info.e[i]); 
    }
    
    //   
