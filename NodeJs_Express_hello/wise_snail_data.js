db6e42edf44e58b529eb870025ac112de99932b3
ivan <ivan0124.tw@gmail.com>
2016-10-01 23:50:52 +0800

    Update hello.js

--- NodeJs_Express_hello/wise_snail_data.js
+++ NodeJs_Express_hello/wise_snail_data.js
@@ -1,3 +1,19 @@
+//Mqtt
+var mqtt = require('mqtt');
+//var client  = mqtt.connect('mqtt://test.mosquitto.org');
+var client  = mqtt.connect('mqtt://127.0.0.1');
+
+client.on('connect', function () {
+  console.log('[WiseSnail] mqtt connect !!!!');
+  client.subscribe('agentinfo');
+  client.publish('agentinfo', 'Hello mqtt');
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer 
+  console.log(message.toString())
+  //client.end()
+})
 
 module.exports = {
   set_connectivity: function() {
