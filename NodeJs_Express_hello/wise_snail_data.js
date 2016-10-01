cab5f5b31b4c0bfb5dfdbecc0e9fc2e21f27685f
ivan <ivan0124.tw@gmail.com>
2016-10-02 00:03:13 +0800

    Update hello.js

--- NodeJs_Express_hello/wise_snail_data.js
+++ NodeJs_Express_hello/wise_snail_data.js
@@ -1,7 +1,8 @@
 //Mqtt
 var mqtt = require('mqtt');
 //var client  = mqtt.connect('mqtt://test.mosquitto.org');
-var client  = mqtt.connect('mqtt://127.0.0.1');
+var client  = mqtt.connect('mqtt://1.2.3.4');
+var mqtt_connected = false;
 
 client.on('connect', function () {
   console.log('[WiseSnail] mqtt connect !!!!');
@@ -11,9 +12,16 @@ client.on('connect', function () {
  
 client.on('message', function (topic, message) {
   // message is Buffer 
-  console.log(message.toString())
+  console.log(message.toString());
   //client.end()
 })
+
+function connectMqtt(){
+  
+    console.log('mqtt_connected=' + mqtt_connected + 'try to connect to mqtt...');
+}
+
+setTimeout(connectMqtt, 1000); 
 
 module.exports = {
   set_connectivity: function() {
