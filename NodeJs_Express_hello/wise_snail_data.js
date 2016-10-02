4ee4613f7591679a0b2bd8503ad65e1aaa921859
ivan <ivan0124.tw@gmail.com>
2016-10-02 02:59:13 -0500

    Update hello.js

--- NodeJs_Express_hello/wise_snail_data.js
+++ NodeJs_Express_hello/wise_snail_data.js
@@ -1,5 +1,7 @@
 //Mqtt
 var mqtt = require('mqtt');
+var HashMap = require('hashmap').HashMap;
+
 client  = mqtt.connect('mqtt://127.0.0.1'); 
 
 client.on('connect', function () {
@@ -10,9 +12,21 @@ client.on('connect', function () {
  
 client.on('message', function (topic, message) {
   // message is Buffer 
-  console.log(message.toString());
+  console.log('topic=' + topic + 'msg=' + message.toString());
+  
+  
+  var sensor_hub_map = new HashMap();
+  var sensor1 = {id: topic.toString(), cap: message.toString() };
+  sensor_hub_map.set('123', sensor1);
+  //
+  var conn = {cap: 'null', sensor_hub: sensor_hub_map };
+  var sen= conn.sensor_hub.get('123');
+  console.log('sen.id=' + tmp_sen.id + ', sen.cap=' + tmp_sen.cap );
   //client.end()
 })
+
+
+function s
 
 
 module.exports = {
