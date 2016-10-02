dd7c8a00cd110e8b537d51dac1779ed45ec32502
ivan <ivan0124.tw@gmail.com>
2016-10-02 16:24:32 +0800

    Update hello.js

--- NodeJs_Express_hello/wise_snail_data.js
+++ NodeJs_Express_hello/wise_snail_data.js
@@ -18,8 +18,13 @@ client.on('message', function (topic, message) {
   //
   var conn=conn_map.get('key1');
   if (typeof conn != 'undefined') {
+    console.log('[wise_snail_data] conn key1 exist');
+    var sen= conn.sensor_hub.get('123');
+    console.log('sen.id=' + sen.cmd + ', sen.cap=' + sen.msg );
+  }
+  else{
+    console.log('[wise_snail_data] conn key1 does nost exist');
     
-    console.log('[wise_snail_data] conn key1 does not exist');
     var sensor_hub_map = new HashMap();
     var sensor1 = { cmd: topic.toString(), msg: message.toString() };
     sensor_hub_map.set('123', sensor1);
@@ -27,12 +32,8 @@ client.on('message', function (topic, message) {
     var conn = {cap: 'null', sensor_hub: sensor_hub_map };
     var sen= conn.sensor_hub.get('123');
     console.log('sen.id=' + sen.cmd + ', sen.cap=' + sen.msg );
-    conn_map.set('key1', conn);
-  }
-  else{
-    console.log('[wise_snail_data] conn key1 exist');
-    //var sen= conn.sensor_hub.get('123');
-    //console.log('sen.id=' + sen.cmd + ', sen.cap=' + sen.msg );
+    conn_map.set('key1', conn);    
+
   }
   //
   //client.end()
