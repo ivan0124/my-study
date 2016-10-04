404f932597d9fae60e5f53366cf8efae0ab04e13
ivan <ivan0124.tw@gmail.com>
2016-10-04 18:06:01 +0800

    Update code

--- NodeJs_Express_hello/wise_snail_data.js
+++ NodeJs_Express_hello/wise_snail_data.js
@@ -263,7 +263,7 @@ function list_info_all_sensor_hub( vgw_id, connName, layer, connType, infoObj ){
       if (infoObj.hasOwnProperty(key)) {
           if ( infoObj[key] === 'SenHubList' ){
              console.log( 'SenHubList :' + infoObj['sv'] ); 
-             var conn = comm_map.get(connName);
+             var conn = conn_map.get(connName);
              if ( conn !== 'undefined' ){
                console.log( connName + 'exist');
              }
