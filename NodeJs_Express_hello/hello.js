20cb8251d10a86e7daac3785fc4741fb83bfa5b8
ivan <ivan0124.tw@gmail.com>
2016-10-04 11:25:30 +0800

    Update code

--- NodeJs_Express_hello/hello.js
+++ NodeJs_Express_hello/hello.js
@@ -230,6 +230,20 @@ function listObj( i, keyStr, jsonObj ){
    */
 }
 
+app.get('/add_json', function (req, res) {
+ var susiString = "{\"susiCommData\":{\"infoSpec\":{\"IoTGW\":{\"WSN\":{\"WSN0\":{\"Info\":\
+                    {\"e\":[{\"n\":\"SenHubList\",\"sv\":\"\",\"asm\":\"r\"},{\"n\":\"Neighbor\",\"sv\":\"\",\"asm\":\"r\"},\
+                    {\"n\":\"Name\",\"sv\":\"WSN0\",\"asm\":\"r\"},{\"n\":\"Health\",\"v\":\"100.000000\",\"asm\":\"r\"},\
+                    {\"n\":\"sw\",\"sv\":\"1.2.1.12\",\"asm\":\"r\"},{\"n\":\"reset\",\"bv\":\"0\",\"asm\":\"rw\"}],\
+                    \"bn\":\"Info\"},\"bn\":\"0007000E40ABCDEF\",\"ver\":1},\"bn\":\"WSN\",\"ver\":1},\"ver\":1}},\
+                    \"commCmd\":2052,\"requestID\":2001,\"agentID\":\"0000000E40ABCDEF\",\"handlerName\":\"general\",\
+                    \"sendTS\":160081020}}";
+  var susiObj = JSON.parse(susiString);
+  
+  susiObj.susiCommData.test= 123;
+  res.send('add_json');
+});
+
 app.get('/restapi/susiCommData/infoSpec/IoTGW', function (req, res) { 
   
   //
