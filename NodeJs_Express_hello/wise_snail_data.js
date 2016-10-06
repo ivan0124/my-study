3dce73906290744788e2fda47686369b289ca23a
ivan <ivan0124.tw@gmail.com>
2016-10-06 10:19:39 +0800

    Update code

--- NodeJs_Express_hello/wise_snail_data.js
+++ NodeJs_Express_hello/wise_snail_data.js
@@ -176,7 +176,6 @@ function getStatusFromMsg( connectMsg ){
   try {
       var msgObj = JSON.parse(connectMsg.toString());
       var status = msgObj.susiCommData.status;
-      console.log('msgObj =' + status);
       if ( status === 1 || status === '1' ){
         return 'on';
       }    
