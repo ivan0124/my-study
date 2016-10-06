8db2f6c2ed7c482517b55774292c057efa4b889f
ivan <ivan0124.tw@gmail.com>
2016-10-06 17:31:49 +0800

    Update code

--- NodeJs_Express_hello/hello.js
+++ NodeJs_Express_hello/hello.js
@@ -341,7 +341,8 @@ app.get('/add_json', function (req, res) {
                     \"sendTS\":160081020}}";
   var susiObj = JSON.parse(susiString);
   
-  susiObj.susiCommData.infoSpec.IoTGW.test1 = {"ZigBee":{"bn":20}};
+  var Name1='ttt';
+  susiObj.susiCommData.infoSpec.IoTGW[Name1] = {"ZigBee":{"bn":20}};
   susiObj.susiCommData.infoSpec.IoTGW.test2 = {"ZigBee":{"bn":20}};
   var string = JSON.stringify(susiObj);
   console.log("string="+string);
