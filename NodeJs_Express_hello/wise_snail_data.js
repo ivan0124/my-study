8d7601436aefd9b692cf058470e9e00c6e6a9af3
ivan <ivan0124.tw@gmail.com>
2016-10-06 11:20:53 +0800

    Update code

--- NodeJs_Express_hello/wise_snail_data.js
+++ NodeJs_Express_hello/wise_snail_data.js
@@ -129,7 +129,7 @@ client.on('message', function (topic, message) {
     case msgType.sen_connect:
       {
           console.log('[' + device_id + ']' + ': sen_connect');
-          var res = sensor_hub_map_get_senhub( device_id, function ( senObj ){ 
+          var res = sensor_hub_map_get_senhub( 'device_id', function ( senObj ){ 
             console.log('[senObj]: ' + senObj );
             senObj.connect = message.toString();
           } );
