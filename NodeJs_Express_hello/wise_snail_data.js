423b0a151959c2b7a086f0425eb6ceee2ab85ba5
ivan <ivan0124.tw@gmail.com>
2016-10-07 11:33:06 +0800

    Update code

--- NodeJs_Express_hello/wise_snail_data.js
+++ NodeJs_Express_hello/wise_snail_data.js
@@ -33,7 +33,7 @@ client.on('message', function (topic, message) {
   console.log('msg=' + message.toString());
   
   try {
-      var jsonObj = JSON.parse(message.toString());
+      var jsonObj = JSON.parse(message);
   } catch (e) {
       console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
       console.error(e);
