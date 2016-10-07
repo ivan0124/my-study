55f25fe7efe26538890fc6d9a7089b61fc2d971b
ivan <ivan0124.tw@gmail.com>
2016-10-07 14:15:08 +0800

    Update code

--- NodeJs_Express_hello/wise_snail.js
+++ NodeJs_Express_hello/wise_snail.js
@@ -95,6 +95,11 @@ function vgw_send_info_spec( vgw_mac ){
             {\"n\":\"reset\",\"bv\":\"0\",\"asm\":\"rw\"}],\"bn\":\"Info\"},\"bn\":\"0007000E40ABCDEF\",\"ver\":1},\
             \"bn\":\"Ethernet\",\"ver\":1},\"ver\":1}},\"commCmd\":2052,\"requestID\":2001,\"agentID\":\"0000000E40ABCDEF\",\
             \"handlerName\":\"general\",\"sendTS\":160081020}}';
+  
+  var InfoMsg = '{\"e\":[{\"n\":\"SenHubList\",\
+            \"sv\":\"\",\"asm\":\"r\"},{\"n\":\"Neighbor\",\"sv\":\"\",\"asm\":\"r\"},{\"n\":\"Name\",\"sv\":\"Ethernet\",\"asm\":\"r\"},\
+            {\"n\":\"Health\",\"v\":\"100.000000\",\"asm\":\"r\"},{\"n\":\"sw\",\"sv\":\"1.2.1.12\",\"asm\":\"r\"},\
+            {\"n\":\"reset\",\"bv\":\"0\",\"asm\":\"rw\"}],\"bn\":\"Info\"}'
    
   var msgObj = JSON.parse(msg);
  
@@ -102,12 +107,7 @@ function vgw_send_info_spec( vgw_mac ){
   msgObj.susiCommData.sendTS = new Date().getTime();
   msgObj.susiCommData.infoSpec.IoTGW['WSN']={};
    msgObj.susiCommData.infoSpec.IoTGW['WSN']['WSN0']={};
-  msgObj.susiCommData.infoSpec.IoTGW['WSN']['WSN0']['Info'] = {e:[{n:'SenHubList',sv:'',asm:'r'},\
-                                                                  {n:'Neighbor',sv:'',asm:'r'},\
-                                                                  {n:'Name',sv:'WSN0',asm:'r'},\
-                                                                  {n:'Health',v:'100.000000',asm:'r'},\
-                                                                  {n:'sw',sv:'1.2.1.12',asm:'r'},\
-                                                                  {n:'reset',bv:'0',asm:'rw'}],bn:'Info'};
+  msgObj.susiCommData.infoSpec.IoTGW['WSN']['WSN0']['Info'] = JSON.parse(InfoMsg);
                          
   msgObj.susiCommData.infoSpec.IoTGW['WSN']['WSN0']['bn'] = conn_id_prefix + vgw_mac;
   msgObj.susiCommData.infoSpec.IoTGW['WSN']['WSN0']['ver'] = 1;
