b2319416a82cdf18dcdd611efaf2d591aa841f46
ivan <ivan0124.tw@gmail.com>
2016-10-07 11:40:26 +0800

    Update code

--- NodeJs_Express_hello/wise_snail_data.js
+++ NodeJs_Express_hello/wise_snail_data.js
@@ -28,10 +28,10 @@ client.on('connect', function () {
  
 client.on('message', function (topic, message) {
   // message is Buffer 
-  var msg=message.toString();
-  //console.log('--------------------------------------------------------------');
- // console.log('topic=' + topic.toString() );
-  //console.log('msg=' + msg);
+  //var msg=message.toString();
+  console.log('--------------------------------------------------------------');
+  console.log('topic=' + topic.toString() );
+  console.log('msg=' + msg);
   
   try {
       var jsonObj = JSON.parse(msg);
