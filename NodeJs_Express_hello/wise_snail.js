d276005dab8c2e33a4e1d9ecc0b7e3ccfa67f04c
ivan <ivan0124.tw@gmail.com>
2016-10-07 09:08:29 +0800

    Update code

--- NodeJs_Express_hello/wise_snail.js
+++ NodeJs_Express_hello/wise_snail.js
@@ -24,9 +24,17 @@ client.on('message', function (topic, message) {
   
 })
 
+function test1(){
+    client.publish('/cagent/admin/0000000E4CABCD99/agentinfoack', '{\"susiCommData\":{\"devID\":\"0000000E4CABCD99\",\"parentID\":\"\",\
+\"hostname\":\"IotGW(CDEF)\",\"sn\":\"000E4CABCD99\",\"mac\":\"000E4CABCD99\",\"version\":\"3.1.23\",\"type\":\"IoTGW\",\"product\":\"\",\
+\"manufacture\":\"\",\"account\":\"\",\"passwd\":\"\",\"status\":1,\"commCmd\":1,\"requestID\":21,\"agentID\":\"0000000E4CABCD99\",\
+\"handlerName\":\"general\",\"sendTS\":{\"$date\":1469512074}}}');
+}
+
 module.exports = {
   test: function() {
     console.log('[wise_snail] test');
+    test1();
     return;
   },
 };
