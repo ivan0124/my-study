8a87ea42f59855918b338239969183ef7f906928
ivan <ivan0124.tw@gmail.com>
2016-10-07 09:03:36 +0800

    Update code

--- NodeJs_Express_hello/wise_snail.js
+++ NodeJs_Express_hello/wise_snail.js
@@ -1,3 +1,28 @@
+var mqtt = require('mqtt');
+
+client  = mqtt.connect('mqtt://127.0.0.1');
+client.queueQoSZero = false;
+
+client.on('connect', function () {
+  console.log('[wise_snail] mqtt connect !!!!');
+})
+
+
+client.on('message', function (topic, message) {
+  // message is Buffer 
+  console.log('--------------------------------------------------------------');
+  console.log('topic=' + topic.toString() );
+  console.log('msg=' + message.toString());
+  
+  try {
+      var jsonObj = JSON.parse(message.toString());
+  } catch (e) {
+      console.error(e);
+      return;
+  }
+  console.log('--------------------------------------------------------------');
+  
+})
 
 module.exports = {
   test: function() {
