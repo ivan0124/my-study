8b8422aea36a2fe3567152aa5baa30a7e0e8a68a
ivan <ivan0124.tw@gmail.com>
2016-10-08 10:00:32 +0800

    Update code

--- NodeJs_Express_hello/wise_snail_data.js
+++ NodeJs_Express_hello/wise_snail_data.js
@@ -460,7 +460,7 @@ function is_ip_valid( ip ){
   if (ip_arr.length !== 4 ){
       return false;
   }
-  
+  /*
   if ( ip === '0.0.0.0'){
     return false;
   }
@@ -468,6 +468,7 @@ function is_ip_valid( ip ){
       ip_arr[2] === 0 && ip_arr[3] === 0){
       return false;
   }
+  */
   
   if ( (ip_arr[0] >= 0 && ip_arr[0] < 256) &&
        (ip_arr[1] >= 0 && ip_arr[1] < 256) &&
