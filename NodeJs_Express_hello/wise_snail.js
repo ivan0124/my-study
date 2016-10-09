a61c0b9c27e909a6956d71d8454f6842704060b3
ivan <ivan0124.tw@gmail.com>
2016-10-09 21:21:47 +0800

    Update code

--- NodeJs_Express_hello/wise_snail.js
+++ NodeJs_Express_hello/wise_snail.js
@@ -339,6 +339,8 @@ module.exports = {
     //
    
     var temp = fs.readFileSync('Temperature.dat', 'utf8');
+    temp.replace(/\r\n|\n/g,'');
+    temp.replace(/\s+/g,'');
     var temp_array = temp.split(',');
     console.log('temp_array.length = ' + temp_array.length);
     for (var i=0 ; i< temp_array.length ; i++){
