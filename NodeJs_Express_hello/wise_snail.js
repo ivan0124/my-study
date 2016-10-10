afdedc184bf32bda5abe2d86c56f63b56c8a10c0
ivan <ivan0124.tw@gmail.com>
2016-10-10 14:25:49 +0800

    Update code

--- NodeJs_Express_hello/wise_snail.js
+++ NodeJs_Express_hello/wise_snail.js
@@ -356,23 +356,24 @@ function sendVGW( mac ){
       infoObj[connObj].info = JSON.parse(fs.readFileSync( CONN_path + 'info.msg', 'utf8'));
       //      
       var senhubfiles = fs.readdirSync(CONN_path);
-      var infoObj = JSON.parse(fs.readFileSync( CONN_path + 'info.msg', 'utf8'));
-      infoObj.e[0].sv = '';
+      //var infoObj = JSON.parse(fs.readFileSync( CONN_path + 'info.msg', 'utf8'));
+      var eObj = infoObj[connObj].info.e[0];
+      eObj.sv = '';
       console.log('senhubfiles.length = ' + senhubfiles.length);
       for (var j=0 ; j< senhubfiles.length ; j++){
         console.log('name = ' + senhubfiles[j]);
         var senhubRegex = new RegExp("^SENSORHUB");
         if( senhubRegex.test(senhubfiles[j]) ){
           console.log('SendorHub name = ' + senhubfiles[j]);
-          /*
-          if ( infoObj.e[0].sv.length === 0 ){
-            infoObj.e[0].sv = senhubfiles[j].split('_')[1];
+          
+          if ( eObj.sv.length === 0 ){
+            eObj.sv = senhubfiles[j].split('_')[1];
           }
           else{
-            infoObj.e[0].sv = infoObj.e[0].sv + ',';
-            infoObj.e[0].sv = infoObj.e[0].sv + senhubfiles[j].split('_')[1];
+            eObj.sv = eObj.sv + ',';
+            eObj.sv = eObj.sv + senhubfiles[j].split('_')[1];
           }
-          */
+          
         }
       }
       //infoObj[connObj].info = infoObj;
