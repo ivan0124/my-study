1a5b5dec941684f747d0426e35d2747003fee3da
ivan <ivan0124.tw@gmail.com>
2016-10-11 14:09:05 +0800

    Update code

--- NodeJs_Express_hello/wise_snail.js
+++ NodeJs_Express_hello/wise_snail.js
@@ -19,7 +19,7 @@ keypress(process.stdin);
 process.stdin.on('keypress', function (ch, key) {
   console.log('got "keypress"', key);
   if (key && key.ctrl && key.name == 'c') {
-    process.stdin.pause();
+    process.exit();
   }
 });
 
