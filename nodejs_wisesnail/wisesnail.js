afa0126d0ec326e0991110739bd8ea80e02d71e3
ivan <ivan0124.tw@gmail.com>
2016-10-11 15:43:54 +0800

    Update code

--- nodejs_wisesnail/wisesnail.js
+++ nodejs_wisesnail/wisesnail.js
@@ -11,7 +11,7 @@ var max_time = 0;
 var timer_interval = 2000;
 
 var keypress = require('keypress');
-
+/*
 // make `process.stdin` begin emitting "keypress" events
 keypress(process.stdin);
 
@@ -26,6 +26,7 @@ process.stdin.on('keypress', function (ch, key) {
 
 process.stdin.setRawMode(true);
 process.stdin.resume();
+*/
 
 function timeout(){
   
