ce764d73e323c68b77035f045ec4998d9ae59533
ivan <ivan0124.tw@gmail.com>
2016-10-11 13:52:49 +0800

    Update code

--- NodeJs_Express_hello/wise_snail.js
+++ NodeJs_Express_hello/wise_snail.js
@@ -14,10 +14,13 @@ var stdin = process.openStdin();
 require('tty').setRawMode(true);    
 
 stdin.on('keypress', function (chunk, key) {
+  /*
   process.stdout.write('Get Chunk: ' + chunk + '\n');
   if (key && key.ctrl && key.name == 'c'){
     process.exit();
   }
+  */
+  console.log('keypress...............');
 });
 
 function timeout(){
