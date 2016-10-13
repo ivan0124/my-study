043b43fa81c5c5927e759dd1d17f3e5abe32b60d
ivan <ivan0124.tw@gmail.com>
2016-10-13 18:07:17 +0800

    Update code

--- NodeJs_Express_hello/wisesnail_msgmgr.js
+++ NodeJs_Express_hello/wisesnail_msgmgr.js
@@ -32,7 +32,7 @@ client.on('connect', function () {
    
 })
 
-client.on('message', mqttMessageCallback);
+client.on('message', mqttMessageCallback(topic, message));
  
 var mqttMessageCallback = function (topic, message){
   // message is Buffer 
