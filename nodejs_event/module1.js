0b4e35dc42177cf65327ba637953954af5a40042
ivan <ivan0124.tw@gmail.com>
2016-10-14 10:03:21 +0800

    Update code

--- nodejs_event/module1.js
+++ nodejs_event/module1.js
@@ -5,9 +5,20 @@ const EventEmitter = require('events');
 var obj = new EventEmitter();
 
 // export the EventEmitter object so others can use it
-module.exports = obj;
 
-var someData = 'this evnent test';
+module.exports = {
+  sayHelloInEnglish: function() {
+    return "HELLO";
+  },
+       
+  sayHelloInSpanish: function() {
+    return "Hola";
+  }
+};
+
+module.exports.eObj = obj;
+
+var someData = 'this event test';
 // other code in the module that does something to trigger events
 // this is just one example using a timer
 setInterval(function() {
