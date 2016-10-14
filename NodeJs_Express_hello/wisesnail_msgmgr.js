51900e12a79571949d94213208c10f3a18c03cac
ivan <ivan0124.tw@gmail.com>
2016-10-14 09:20:22 +0800

    Update code

--- NodeJs_Express_hello/wisesnail_msgmgr.js
+++ NodeJs_Express_hello/wisesnail_msgmgr.js
@@ -5,19 +5,6 @@ var vgw_map = new HashMap();
 var sensorHubMap = new HashMap();
 var connectivityMap = new HashMap();
 
-// create EventEmitter object
-var EventEmitter = require('events');
-var eventObj = new EventEmitter();
-
-// export the EventEmitter object so others can use it
-module.exports = eventObj;
-
-// other code in the module that does something to trigger events
-// this is just one example using a timer
-setInterval(function() {
-    eventObj.emit("someEvent", someData);
-}, 5000);
-
 const msgType = { error: -1, unknown: 0,
                   vgw_connect: 1, vgw_os_info: 2, vgw_info_spec: 3, vgw_willmessage: 4,
                   vgw_disconnect: 5, vgw_info: 6,
