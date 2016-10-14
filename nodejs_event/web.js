5dc5a9c9d0a0e863d69a5746bbd2a44b9c2d212d
ivan <ivan0124.tw@gmail.com>
2016-10-14 10:49:35 +0800

    Update code

--- nodejs_event/web.js
+++ nodejs_event/web.js
@@ -3,12 +3,12 @@ var express = require('express');
 var app = express();
 var m1 = require('./module1.js');
 
-m1.eObj.on(EVENT.eConnectivity_Capability, function(data) {
+m1.notification .on(EVENT.eConnectivity_Capability, function(data) {
     // process data when someEvent occurs
   console.log('[RECEIVE] EVENT.eConnectivity_Capability, data = ' + data);
 });
 
-m1.eObj.on(EVENT.eConnectivity_UpdateData , function(data) {
+m1.notification .on(EVENT.eConnectivity_UpdateData , function(data) {
     // process data when someEvent occurs
   console.log('EVENT.eConnectivity_UpdateData, data = ' + data);
 });
