5475d21999464d3817304f1cb7108a7ff5f0a679
ivan <ivan0124.tw@gmail.com>
2016-10-14 09:17:02 +0800

    Update code

--- NodeJs_Express_hello/module1.js
+++ NodeJs_Express_hello/module1.js
@@ -1,11 +1,14 @@
+// module1.js
+// module that has events
+const EventEmitter = require('events');
+// create EventEmitter object
+var obj = new EventEmitter();
 
-module.exports = {
-  sayHelloInEnglish: function() {
-    console.log('[greetings] sayHelloInEnglish');
-    return "HELLO";
-  },
-       
-  sayHelloInSpanish: function() {
-    return "Hola";
-  }
-};
+// export the EventEmitter object so others can use it
+module.exports = obj;
+
+// other code in the module that does something to trigger events
+// this is just one example using a timer
+setInterval(function() {
+    obj.emit("someEvent", someData);
+}, 3000);
