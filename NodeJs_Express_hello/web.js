266bb0a149161c0b2e2abbd0e96472f0f2c7f282
ivan <ivan0124.tw@gmail.com>
2016-10-14 09:21:24 +0800

    Update code

--- NodeJs_Express_hello/web.js
+++ NodeJs_Express_hello/web.js
@@ -6,7 +6,7 @@ var HashMap = require('hashmap').HashMap;
 var map = new HashMap();
 var app = express();
 
-var m1 = require('module1.js');
+var m1 = require('./module1.js');
 m1.on("someEvent", function(data) {
     // process data when someEvent occurs
   console.log('[RECEIVE] someEvent data = ' + data);
