25ce8250a5d7e91e609b5220ff2ccb68cdf21f3b
ivan <ivan0124.tw@gmail.com>
2016-10-14 10:28:43 +0800

    Update code

--- nodejs_event/wisesnail_event.js
+++ nodejs_event/wisesnail_event.js
@@ -1,13 +1,7 @@
-var express = require('express');
-var app = express();
-var m1 = require('./module1.js');
+const eConnectivity_Capability = 1;
+cosnt eConnectivity_UpdateData = 2;
 
-m1.on("someEvent", function(data) {
-    // process data when someEvent occurs
-  console.log('[RECEIVE] someEvent data = ' + data);
-});
-
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!!!!');
-});
+module.exports = {
+    eConnectivity_Capability: eConnectivity_Capability,
+    eConnectivity_UpdateData: eConnectivity_UpdateData,
+};
