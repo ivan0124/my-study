c9ebb6f62816110127b446e867462eb4a43bfdf5
ivan0124 <ivan0124.tw@gmail.com>
2016-10-14 10:24:32 +0800

    add files

---
+++ nodejs_event/wisesnail_event.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var m1 = require('./module1.js');
+
+m1.on("someEvent", function(data) {
+    // process data when someEvent occurs
+  console.log('[RECEIVE] someEvent data = ' + data);
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!!!!');
+});
