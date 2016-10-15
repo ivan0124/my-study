941d9f7f420a6c6f3d2bd4d69abc827a130022b7
ivan <ivan0124.tw@gmail.com>
2016-10-15 20:40:41 +0800

    Update node

--- NodeJs_C_addon/pow.cpp
+++ NodeJs_C_addon/pow.cpp
@@ -2,6 +2,30 @@
 #include <cmath>
 #include <nan.h>
 
+int hello(double a, double b){
+    printf("a = %f, b = %f\n", a, b);
+    return 99;
+}
+
+void hello(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+
+    if (info.Length() < 2) {
+        Nan::ThrowTypeError("Wrong number of arguments");
+        return;
+    }
+
+    if (!info[0]->IsNumber() || !info[1]->IsNumber()) {
+        Nan::ThrowTypeError("Both arguments should be numbers");
+        return;
+    }
+
+    double arg0 = info[0]->NumberValue();
+    double arg1 = info[1]->NumberValue();
+    v8::Local<v8::Number> num = Nan::New(hello(arg0, arg1));
+
+    info.GetReturnValue().Set(num);
+}
+
 void Pow(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 
     if (info.Length() < 2) {
@@ -24,6 +48,9 @@ void Pow(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 void Init(v8::Local<v8::Object> exports) {  
     exports->Set(Nan::New("pow").ToLocalChecked(),
                  Nan::New<v8::FunctionTemplate>(Pow)->GetFunction());
+    
+    exports->Set(Nan::New("hello").ToLocalChecked(),
+                 Nan::New<v8::FunctionTemplate>(hello)->GetFunction());    
 }
 
 NODE_MODULE(pow, Init) 
