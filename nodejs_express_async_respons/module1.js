31c3ffa3effeb930f9d81355129f9e62065688af
ivan <ivan0124.tw@gmail.com>
2016-10-20 08:21:26 -0500

    Update code

--- nodejs_express_async_respons/module1.js
+++ nodejs_express_async_respons/module1.js
@@ -1,15 +1,15 @@
 // module1.js
-// module that has events
-const EventEmitter = require('events');
-// create EventEmitter object
-var obj = new EventEmitter();
 
-// export the EventEmitter object so others can use it
-module.exports = obj;
+module.exports = {
+  sayHelloInEnglish: function() {
+    return "HELLO";
+  },
+       
+  sayHelloInSpanish: function() {
+    return "Hola";
+  }
+}
 
-var someData = 'this evnent test';
-// other code in the module that does something to trigger events
-// this is just one example using a timer
 setInterval(function() {
-    obj.emit("someEvent", someData);
+   
 }, 3000);
