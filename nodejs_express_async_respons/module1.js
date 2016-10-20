ac99820d0e19aa4919a172f2d42311fb5f06f76f
ivan <ivan0124.tw@gmail.com>
2016-10-21 00:15:15 +0800

    Update code

--- nodejs_express_async_respons/module1.js
+++ nodejs_express_async_respons/module1.js
@@ -1,10 +1,20 @@
 // module1.js
 
+var i=0;
+var flag = 0;
+
 module.exports = {
   sayHelloInEnglish: function( query, callback) {
     
-    setTimeout(function () {
-       callback('callbak from module1');
+    i = 0;
+    setInterval((function () {
+       if ( flag == 1 ){
+         callback('callbak from module1. i = ' + i);
+       }
+       else{
+         console.log('wait. i = ' + i);
+       }
+       i++
     }, 3000);
     return "HELLO";
   },
