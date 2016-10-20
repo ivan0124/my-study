5643ed017fd2594e45f409d2bb11e5ae076b556a
ivan <ivan0124.tw@gmail.com>
2016-10-20 21:38:59 +0800

    Update code

--- nodejs_express_async_respons/module1.js
+++ nodejs_express_async_respons/module1.js
@@ -1,13 +1,15 @@
 // module1.js
 
-var callbackTimer = function(callback){
-  callback('callbak from module1');
+var CallBack
+var callbackTimer = function(){
+  CallBack('callbak from module1');
 }
 
 module.exports = {
   sayHelloInEnglish: function( query, callback) {
     
-    setInterval( callbackTimer(callback), 3000);    
+    CallBack = callback;
+    setInterval( callbackTimer(), 3000);    
     
     return "HELLO";
   },
