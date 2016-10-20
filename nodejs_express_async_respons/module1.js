47785e98c8919ba4e466ba953e4cadd569d69659
ivan <ivan0124.tw@gmail.com>
2016-10-21 00:35:39 +0800

    Update code

--- nodejs_express_async_respons/module1.js
+++ nodejs_express_async_respons/module1.js
@@ -4,7 +4,27 @@ var i=0;
 var flag = 0;
 var timerObj;
 
+var set = function( query, callback) {
+    
+    i = 0;
+    timerObj = setInterval(function () {
+       
+       if ( flag === 1 || i > 6 ){
+         clearInterval(timerObj); 
+         callback('callbak from module1. i = ' + i);
+       }
+       else{
+         console.log('wait. i = ' + i);
+       }
+       
+       i++
+    }, 500);
+    return;
+  }
+
 module.exports = {
+  wsnset = set;
+  /*
   wsnset: function( query, callback) {
     
     i = 0;
@@ -22,7 +42,7 @@ module.exports = {
     }, 500);
     return;
   },
-       
+  */     
 }
 
 
