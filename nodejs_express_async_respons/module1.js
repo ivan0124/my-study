0e61ccf06d9f96cd6de456d4f8e65974d6903303
ivan <ivan0124.tw@gmail.com>
2016-10-21 00:55:00 +0800

    Update code

--- nodejs_express_async_respons/module1.js
+++ nodejs_express_async_respons/module1.js
@@ -3,9 +3,16 @@
 var i=0;
 var flag = 0;
 var timerObj;
+var setRunning = false ; 
 
 var set = function( query, callback) {
     
+  if ( setRunning === true ){
+    callback('callbak from module1. set is Running, set fail');
+    return;
+  }
+  
+  setRunning = true;  
   i = 0;
   //publish mqtt here  
   console.log(query);
@@ -15,6 +22,7 @@ var set = function( query, callback) {
     if ( flag === 1 || i > 100 ){
       clearInterval(timerObj); 
       callback('callbak from module1. i = ' + i + ', flag = ' + flag);
+      setRunning = false;
     }
     else{
       console.log('wait... i = ' + i + ', flag = ' + flag);
