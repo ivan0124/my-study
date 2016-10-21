7a7646778d83228c02f464c8c387ad61379f5bbb
ivan0124 <ivan0124.tw@gmail.com>
2016-10-21 09:08:03 +0800

    update code

--- nodejs_express_async_respons/module1.js
+++ nodejs_express_async_respons/module1.js
@@ -1,9 +1,43 @@
 // module1.js
+var Mqtt = require('mqtt');
+var Client  = Mqtt.connect('mqtt://172.22.214.60');
+Client.queueQoSZero = false;
+var mqttConnectCallback =  function () {
+
+  console.log('[module1] Mqtt connect !!!!');
+  Client.subscribe('/cagent/admin/+/agentinfoack');
+  //Client.subscribe('/cagent/admin/+/willmessage');
+  //Client.subscribe('/cagent/admin/+/agentactionreq');
+  //Client.subscribe('/cagent/admin/+/deviceinfo'); 
+   
+}
+
+var mqttMessageCallback = function (topic, message){
+}
+
 
 var i=0;
 var flag = 0;
 var timerObj;
-var setRunning = false ; 
+var setRunning = false ;
+var responseData;
+
+var mqttConnectCallback =  function () {
+
+  console.log('[module1] Mqtt connect !!!!');
+  Client.subscribe('/cagent/admin/+/agentinfoack');
+  //Client.subscribe('/cagent/admin/+/willmessage');
+  //Client.subscribe('/cagent/admin/+/agentactionreq');
+  //Client.subscribe('/cagent/admin/+/deviceinfo'); 
+   
+}
+
+var mqttMessageCallback = function (topic, message){
+  console.log('[module1] Mqtt message !!!!');
+  responseData = message.toString();
+  flag = 1;
+}
+ 
 
 var set = function( query, callback) {
     
@@ -14,13 +48,14 @@ var set = function( query, callback) {
   
   setRunning = true;  
   i = 0;
+  flag = 0;
   //publish mqtt here  
     
   timerObj = setInterval(function () {
       
     if ( flag === 1 || i > 100 ){
       clearInterval(timerObj); 
-      callback('callbak from module1. set OK. i = ' + i + ', flag = ' + flag);
+      callback('callbak from module1. set OK. i = ' + i + ', flag = ' + flag + ', responseData =\n' + responseData);
       setRunning = false;
     }
     else{
@@ -37,4 +72,5 @@ module.exports = {
   wsnset: set,  
 }
 
-
+Client.on('connect', mqttConnectCallback );
+Client.on('message', mqttMessageCallback);
