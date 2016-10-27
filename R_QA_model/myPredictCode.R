8c1b0d465556887ed4e1f4728908b60fdd57d928
ivan0124 <ivan0124.tw@gmail.com>
2016-10-27 07:01:12 +0000

    update code

--- R_QA_model/myPredictCode.R
+++ R_QA_model/myPredictCode.R
@@ -1,6 +1,5 @@
 
-print("1====================================\n")
-
+#load trained model
 load("model.RData")
 exp(train.result$coef)
 
@@ -11,10 +10,13 @@ exp(confint(train.result))
 test_data =read.table("./test_data.txt", header =T)
 pred = predict(train.result, newdata =test_data, type ="response")
 for(j in 1:nrow(test_data)) {
-  if (pred[j] <=0.385) pred[j]=0  else  pred[j]=1
+
+  if (pred[j] <=0.385){
+    pred[j]=0
+    cat("===> predict result = GOOD.\n")
+  } 
+  else{
+    pred[j]=1
+    cat("===> predict result = FAIL.\n")
+  }
 }
-print(pred)
-print("2====================================\n")
-pred = round(pred) 
-print(pred)
-print("3====================================\n")
