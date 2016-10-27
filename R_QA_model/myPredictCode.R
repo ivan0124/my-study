421167baaeeb7c1334d625bcaee1cfb3855df807
ivan0124 <ivan0124.tw@gmail.com>
2016-10-27 08:44:11 +0000

    update code

--- R_QA_model/myPredictCode.R
+++ R_QA_model/myPredictCode.R
@@ -8,13 +8,14 @@ confint(train.result)
 exp(confint(train.result))
 
 test_data =read.table("./test_data.txt", header =T)
-cut_value =read.table("./cut_value.txt", header =T)
+cut_value_file =read.table("./cut_value.txt", header =T)
 #print(cut_value[1])
+cat("cut_value_file$cut_value[1] = ", cut_value_file$cut_value[1], "\n")
 
 pred = predict(train.result, newdata =test_data, type ="response")
 for(j in 1:nrow(test_data)) {
 
-  if (pred[j] <= cut_value[1]){
+  if (pred[j] <= cut_value_file$cut_value[1]){
     pred[j]=0
     cat("===> predict result = GOOD.\n")
   } 
