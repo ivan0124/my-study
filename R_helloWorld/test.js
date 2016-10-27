bd4f2aa09c0f346d742651f3fd8aeddedddfe99a
ivan0124 <ivan0124.tw@gmail.com>
2016-10-27 01:54:02 +0000

    update code

--- R_helloWorld/test.js
+++ R_helloWorld/test.js
@@ -1,6 +1,26 @@
 
-var R = require("r-script");
+var spawn = require('child_process').spawn
+var env = process.env
 
-var out = R("ex-sync.R").data("hello world", 20).callSync();
-  
-console.log(out);
+var opts = { cwd: './',
+             env: process.env
+           }
+// can modify child env with needed key-value pairs
+opts.env['YEAR']=2013
+opts.env['BASIN']='SC'
+
+var RCall = ['--no-restore','--no-save','a.R']
+var R  = spawn('Rscript', RCall, opts)
+
+    R.on('exit',function(code){
+        console.log('got exit code: '+code)
+        if(code==1){
+            // do something special
+        }else{
+        }
+        return null
+    })
+
+R.stdout.on('data', (data) => {
+  console.log('stdout:' + data);
+});
