1baae84dd70439238900a4b0e7b4da23bfa8de16
ivan0124 <ivan0124.tw@gmail.com>
2016-10-28 00:26:01 +0000

    update code

---
+++ R_QA_ML_model/app.js
@@ -0,0 +1,24 @@
+
+var spawn = require('child_process').spawn
+
+
+var env = process.env
+var opts = { cwd: './',
+             env: process.env
+           }
+
+var RCall = ['--no-restore','--no-save','PredictionModel.R','111,222,333']
+var R  = spawn('Rscript', RCall, opts)
+
+R.on('exit',function(code){
+  console.log('got exit code: '+code)
+  if(code==1){
+            // do something special
+  }else{
+  }
+  return null
+})
+
+R.stdout.on('data', (data) => {
+  console.log('stdout:' + data);
+});
