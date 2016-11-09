f6383c72a557751dab4f73da3a314c0f771cd373
ivan0124 <ivan0124.tw@gmail.com>
2016-11-09 17:51:34 +0800

    update code

--- perl_curl/perl_curl_put.pl
+++ perl_curl/perl_curl_put.pl
@@ -2,7 +2,7 @@
 
     #open(CURL,'curl --connect-timeout 3 -H "Content-Type: application/json" -X GET http://admin:admin@172.22.12.7/restapi/APIInfoMgmt/APIS |') || die "Failed: $!\n";
     
-    open(CURL,'curl --connect-timeout 3 -H "Content-Type: application/json" -X PUT -d \'{"sv":"curl_put_1234"}\' http://172.22.213.174:3000/restapi/wsnmanage/SenHub/0017000E4C000000/SenHub/Info/Name |') || die "Failed: $!\n";
+    open(CURL,'curl --connect-timeout 3 -H "Content-Type: application/json" -X PUT -d \'{"sv":"curl_put_9999"}\' http://admin:admin@172.22.12.7/restapi/wsnmanage/SenHub/0017000E4C000000/SenHub/Info/Name |') || die "Failed: $!\n";
     my $content;
     
     for($i=0; $line=<CURL>; $i++) {
