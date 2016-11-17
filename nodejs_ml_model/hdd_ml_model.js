f5259fa2389ae4d819f0eb568674cde7901b91c4
ivan0124 <ivan0124.tw@gmail.com>
2016-11-17 14:26:22 +0800

    update code

--- nodejs_ml_model/hdd_ml_model.js
+++ nodejs_ml_model/hdd_ml_model.js
@@ -63,11 +63,21 @@ client.on('message', function (topic, message) {
       console.error(e);
       return;
   }
+
+  var outputObj = {};
+  outputObj.featureList = 'failure ';
+  outputObj.featureVal = '1 ';
+
+  listRESTfulObj( jsonObj, outputObj );
+  console.log('featureList =' + outputObj.featureList);
+  console.log('featureVal =' + outputObj.featureVal);
+
   console.log('----------------------------------------------------------------------------');
   
   sendToMqttBroker('/ML_HDD/12345/predict_result', 'ML_model response');
   
-  var feature_data ='failure smart5 smart9 smart187 smart192 smart194 smart197 smart198\n1 8 1761 4 0 30 0 0'
+  //var feature_data ='failure smart5 smart9 smart187 smart192 smart194 smart197 smart198\n1 8 1761 4 0 30 0 0'
+  var feature_data = outputObj.featureList +'\n' + outputObj.featureVal;
   fs.writeFile("./Feature.data", feature_data, function(err) {
     if(err) {
       return console.log(err);
@@ -86,6 +96,37 @@ function sendToMqttBroker(topic, message){
   
   client.publish(topic, message);
 }
+
+
+function listRESTfulObj( jsonObj, outputObj ){
+  
+  for (key in jsonObj) {
+    if (jsonObj.hasOwnProperty(key)) {
+      console.log( 'key =======>' + key + ', jsonKeyVal=======>' + JSON.stringify(jsonObj[key]));
+      if ( key === 'smart5' || key === 'smart9' || key === 'smart187' ||
+           key === 'smart192' || key === 'smart194' || key === 'smart197' ||
+           key === 'smart198' ){
+        outputObj.featureList += key;
+        outputObj.featureList += ' ';
+        outputObj.featureVal += JSON.stringify(jsonObj[key]);
+        outputObj.featureVal += ' ';
+      }
+    }
+  }
+  //
+  for (key in jsonObj) {
+    if (jsonObj.hasOwnProperty(key)) {
+      if (typeof jsonObj[key] === 'object' ){
+        listRESTfulObj( jsonObj[key], outputObj);
+      }
+    }
+  }
+
+  return;
+
+}
+
+
 
 function sendVGWInfoSpec( msgObj ){
   //
