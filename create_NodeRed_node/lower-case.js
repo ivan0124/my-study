efd86f1ce637885a85a9dbd338a44839d8d505aa
ivan0124 <ivan0124.tw@gmail.com>
2016-12-13 18:03:24 +0800

    update code

--- create_NodeRed_node/lower-case.js
+++ create_NodeRed_node/lower-case.js
@@ -1,10 +1,12 @@
 module.exports = function(RED) {
     function LowerCaseNode(config) {
         RED.nodes.createNode(this,config);
+
         var node = this;
         this.on('input', function(msg) {
             msg.payload = msg.payload.toLowerCase();
-            console.log('my test');
+            console.log('name========================> ' + config.name);
+            console.log('my_test ========================> ' + config.test);
             node.send(msg);
         });
     }
