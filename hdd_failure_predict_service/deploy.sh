a77cdc4191e257655164f09b252096f2e4bb8534
ivan <ivan0124.tw@gmail.com>
2017-01-10 17:17:39 +0800

    Update file

--- hdd_failure_predict_service/deploy.sh
+++ hdd_failure_predict_service/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MQTT_IMAGE=advigw4x86/mqtt-bus
 MQTT_CONTAINER=advigw-mqtt-bus
-HDD_FAILURE_PREDICT_IMAGE=advigw4x86/hdd-failure-predict
+HDD_FAILURE_PREDICT_IMAGE=ivan0124tw/docker_hdd_failure_predict_service
 HDD_FAILURE_PREDICT_CONTAINER=hdd-failure-predict
 ADVANTECH_NET=advigw_network
 
