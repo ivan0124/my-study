75623cdee12816732ddb27108a118a9e3d1566b5
ivan0124 <ivan0124.tw@gmail.com>
2017-01-16 05:53:14 +0000

    update javaScript code

--- HDDPMQ_nodeRed/lower-case.js
+++ HDDPMQ_nodeRed/lower-case.js
@@ -1,14 +1,31 @@
 module.exports = function(RED) {
+
+    var mqtt = require('mqtt');
+
     function LowerCaseNode(config) {
         RED.nodes.createNode(this,config);
 
         var node = this;
+        this.status({fill:"red",shape:"dot",text:"disconnected"});
+
         this.on('input', function(msg) {
             msg.payload = msg.payload.toLowerCase();
             console.log('name========================> ' + config.name);
             console.log('my_test ========================> ' + config.test);
             node.send(msg);
         });
+
+	this.connect = function () {
+	  console.log('[HDDPMQ] node connecting...');
+	  node.client = mqtt.connect('mqtt://172.22.212.92');
+	  node.client.queueQoSZero = false;
+	  node.client.on('connect', function () {
+	    console.log('[HDDPMQ] node.clinet.on--> connected');
+            node.status({fill:"green",shape:"dot",text:"connected"});
+	  });
+	}
+
+        node.connect();
     }
     RED.nodes.registerType("lower-case",LowerCaseNode);
 }
